650baa76c43ec8d951287dcfc46974777a53dcab
lexagreat <9608798010@mail.ru>
2024-07-03 14:09:03 +0300

    fix

--- dist/js/app.js
+++ dist/js/app.js
@@ -431,17 +431,17 @@ function accordion(linkSelector, contentSelector) {
          let openLink = openLinks[i];
          openLink.addEventListener("click", () => {
             // все прячем
-            for (let j = 0; j < contents.length; j++) {
-               // если хоть один открывается - return
-               if (contents[j].classList.contains("collapsing")) {
-                  return;
-               } // Иначе
-               // все прячем
-               slideHide(contents[j], 500);
-            }
-            for (let j = 0; j < openLinks.length; j++) {
-               openLinks[j].classList.remove("active");
-            }
+            // for (let j = 0; j < contents.length; j++) {
+            //    // если хоть один открывается - return
+            //    if (contents[j].classList.contains("collapsing")) {
+            //       return;
+            //    } // Иначе
+            //    // все прячем
+            //    slideHide(contents[j], 500);
+            // }
+            // for (let j = 0; j < openLinks.length; j++) {
+            //    openLinks[j].classList.remove("active");
+            // }
             // записываем в переменную нужный таб
             let content = openLink.nextElementSibling;
             // работаем с классами линка
@@ -449,6 +449,7 @@ function accordion(linkSelector, contentSelector) {
                return;
             } else if (content.classList.contains("collapse_show")) {
                openLink.classList.remove("active");
+               slideHide(content, 500);
             } else {
                openLink.classList.add("active");
             }
