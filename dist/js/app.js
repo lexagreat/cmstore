0b1552e18b0e93438ce9612d193a93cc10dd250d
lexagreat <9608798010@mail.ru>
2024-07-03 12:49:20 +0300

    add faq page

--- dist/js/app.js
+++ dist/js/app.js
@@ -23,6 +23,10 @@ document.addEventListener("DOMContentLoaded", () => {
    initSalesSwiper();
    initProductsSwiper();
    initNewsSwiper();
+
+   //faq
+   useNavigation();
+   accordion(".faq-item__header", ".faq-item__collapse");
 });
 
 function initHeroSwiper() {
@@ -311,4 +315,213 @@ function initNewsSwiper() {
          },
       },
    });
+}
+
+function useNavigation() {
+   let headerHeight = document
+      .querySelector(".header")
+      ?.getBoundingClientRect().height;
+   let navbar = document.querySelector(".faq-navigation");
+   let navbarHeight = navbar?.getBoundingClientRect().height;
+   let sticky = navbar.offsetTop;
+   window.addEventListener("scroll", makeNavSticky);
+   makeNavSticky();
+   pageNavigation();
+
+   // onBeforeUnmount(() => {
+   //    window.removeEventListener("scroll", makeNavSticky);
+   // });
+   function makeNavSticky() {
+      if (window.scrollY + headerHeight >= sticky) {
+         navbar.classList.add("sticky");
+         navbar.classList.add("lock-padding");
+      } else {
+         navbar.classList.remove("sticky");
+         navbar.classList.remove("lock-padding");
+      }
+   }
+
+   function pageNavigation() {
+      const navSwiper = new Swiper(".faq-navigation .swiper", {
+         speed: 800,
+         spaceBetween: 8,
+         slidesPerView: "auto",
+      });
+      const links = document.querySelectorAll(".faq-navigation__link");
+      const sections = document.querySelectorAll(".faq-section");
+      if (!links.length) {
+         return;
+      }
+      links.forEach((link, index) => {
+         link.addEventListener("click", () => {
+            sections.forEach((section, i) => {
+               if (index === i) {
+                  window.scrollBy({
+                     top:
+                        section.getBoundingClientRect().top -
+                        headerHeight -
+                        navbarHeight,
+                     behavior: "smooth",
+                  });
+               }
+            });
+         });
+      });
+
+      const callback = (entries, observer) => {
+         entries.forEach((entry) => {
+            if (!entry.isIntersecting && entry.boundingClientRect.top < 0) {
+               let section = entry.target?.nextElementSibling;
+               if (!section || !section.getAttribute("data-scroll")) {
+                  return;
+               }
+               let id = section.getAttribute("data-scroll");
+               links.forEach((link) => {
+                  link.classList.remove("active");
+                  if (link.getAttribute("data-scroll") === id) {
+                     link.classList.add("active");
+                     // navSwiper.scrollTo(id)
+                     navSwiper.slideTo(id);
+                  }
+               });
+            } else {
+               if (entry.boundingClientRect.top < 0) {
+                  let section = entry.target;
+                  if (!section || !section.getAttribute("data-scroll")) {
+                     return;
+                  }
+                  let id = section.getAttribute("data-scroll");
+                  links.forEach((link) => {
+                     link.classList.remove("active");
+                     if (link.getAttribute("data-scroll") === id) {
+                        link.classList.add("active");
+                        navSwiper.slideTo(id);
+                     }
+                  });
+               }
+            }
+         });
+      };
+
+      const options = {
+         // root: по умолчанию window, но можно задать любой элемент-контейнер
+         rootMargin: "-150px 0px 0px 0px",
+         threshold: 0,
+      };
+
+      const observer = new IntersectionObserver(callback, options);
+
+      sections.forEach((section) => observer.observe(section));
+   }
+}
+
+function accordion(linkSelector, contentSelector) {
+   // получаем линки
+   const openLinks = document.querySelectorAll(`${linkSelector}`);
+   // контенты
+   const contents = document.querySelectorAll(`${contentSelector}`);
+   if (openLinks.length > 0) {
+      for (let i = 0; i < openLinks.length; i++) {
+         let openLink = openLinks[i];
+         openLink.addEventListener("click", () => {
+            // все прячем
+            for (let j = 0; j < contents.length; j++) {
+               // если хоть один открывается - return
+               if (contents[j].classList.contains("collapsing")) {
+                  return;
+               } // Иначе
+               // все прячем
+               slideHide(contents[j], 500);
+            }
+            for (let j = 0; j < openLinks.length; j++) {
+               openLinks[j].classList.remove("active");
+            }
+            // записываем в переменную нужный таб
+            let content = openLink.nextElementSibling;
+            // работаем с классами линка
+            if (content.classList.contains("collapsing")) {
+               return;
+            } else if (content.classList.contains("collapse_show")) {
+               openLink.classList.remove("active");
+            } else {
+               openLink.classList.add("active");
+            }
+            // показываем нужный
+            slideShow(content, 500);
+         });
+      }
+   }
+}
+
+function slideShow(el, duration = 500) {
+   // завершаем работу метода, если элемент содержит класс collapsing или collapse_show
+   if (
+      el.classList.contains("collapsing") ||
+      el.classList.contains("collapse_show")
+   ) {
+      return;
+   }
+   // удаляем класс collapse
+   el.classList.remove("collapse");
+   // сохраняем текущую высоту элемента в константу height (это значение понадобится ниже)
+   const height = el.offsetHeight;
+   // устанавливаем высоте значение 0
+   el.style["height"] = 0;
+   // не отображаем содержимое элемента, выходящее за его пределы
+   el.style["overflow"] = "hidden";
+   // создание анимации скольжения с помощью CSS свойства transition
+   el.style["transition"] = `height ${duration}ms ease`;
+   // добавляем класс collapsing
+   el.classList.add("collapsing");
+   // получим значение высоты (нам этого необходимо для того, чтобы просто заставить браузер выполнить перерасчет макета, т.к. он не сможет нам вернуть правильное значение высоты, если не сделает это)
+   el.offsetHeight;
+   // установим в качестве значения высоты значение, которое мы сохранили в константу height
+   el.style["height"] = `${height}px`;
+   // по истечении времени анимации this._duration
+   window.setTimeout(() => {
+      // удалим класс collapsing
+      el.classList.remove("collapsing");
+      // добавим классы collapse и collapse_show
+      el.classList.add("collapse");
+      el.classList.add("collapse_show");
+      // удалим свойства height, transition и overflow
+      el.style["height"] = "";
+      el.style["transition"] = "";
+      el.style["overflow"] = "";
+   }, duration);
+}
+function slideHide(el, duration = 500) {
+   // завершаем работу метода, если элемент содержит класс collapsing или collapse_show
+   if (
+      el.classList.contains("collapsing") ||
+      !el.classList.contains("collapse_show")
+   ) {
+      return;
+   }
+   // установим свойству height текущее значение высоты элемента
+   el.style["height"] = `${el.offsetHeight}px`;
+   // получим значение высоты
+   el.offsetHeight;
+   // установим CSS свойству height значение 0
+   el.style["height"] = 0;
+   // обрежем содержимое, выходящее за границы элемента
+   el.style["overflow"] = "hidden";
+   // добавим CSS свойство transition для осуществления перехода длительностью this._duration
+   el.style["transition"] = `height ${duration}ms ease`;
+   // удалим классы collapse и collapse_show
+   el.classList.remove("collapse");
+   el.classList.remove("collapse_show");
+   // добавим класс collapsing
+   el.classList.add("collapsing");
+   // после завершения времени анимации
+   window.setTimeout(() => {
+      // удалим класс collapsing
+      el.classList.remove("collapsing");
+      // добавим класс collapsing
+      el.classList.add("collapse");
+      // удалим свойства height, transition и overflow
+      el.style["height"] = "";
+      el.style["transition"] = "";
+      el.style["overflow"] = "";
+   }, duration);
 }
